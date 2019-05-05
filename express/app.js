7386d38fd9ce17f729b952387e12aaea18924741
Dan <ds23@hotmail.co.uk>
2019-05-05 21:18:15 +0100

    Revert

--- express/app.js
+++ express/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-/* app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
- */
+
 app.get('/', (req, res) => {
     res.send('NodeJS app');
 });
@@ -36,9 +36,8 @@ app.get('/random', (req, res) => {
 
     console.log(randomImageNumbers, typeof(randomImageNumbers) + ' ' + outputText, typeof(outputText));;
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
+    //res.header("Access-Control-Allow-Origin", "*");
+    
     res.json({
         'numbers': randomImageNumbers, 
         'text': outputText
