31b7c0953f62471e48d3af33a08d847e0d232bc6
Dan <ds23@hotmail.co.uk>
2019-05-06 10:32:30 +0100

    Install cors npm module and add it as middleware

--- express/app.js
+++ express/app.js
@@ -1,17 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.use(function (req, res, next) { 
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization'); 
-    res.setHeader('Access-Control-Allow-Credentials', true); 
-    if ('OPTIONS' === req.method) { 
-        res.send(204); 
-    } else { 
-        next(); 
-    } 
-}); 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('NodeJS app');
